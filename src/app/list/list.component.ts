3599e16271507cf5cbc3761c2e35342930da3f6b
dreiv <dreivoicu@gmail.com>
2017-05-24 22:35:33 +0300

    made it so only the first click gets taken into consideration for resource download

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, Renderer2, ViewChildren } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements AfterViewInit {
   private response = [
     { title: "Potoci", count: 13 },
     { title: "Molfaitori", count: 7
@@ -29,19 +28,29 @@ export class ListComponent {
       count: 9,
       hasSubHits: true,
     },
-  ]
+  ];
 
-  constructor() { }
+  @ViewChildren('checkbox') checkboxList: QueryList<ElementRef>;
 
-  toggleSubHits(event, hit) {
-    const element: HTMLInputElement = event.target;
+  constructor(private renderer: Renderer2) { }
 
-    element.disabled = true;
-    Observable.of(this.response)
-        .delay(2000)
-        .subscribe( function(x) {
-          hit.subHits = x;
-          element.disabled = false
-        })
+  ngAfterViewInit(): void {
+    this.checkboxList.forEach(component => {
+      const element = component.nativeElement;
+      const index:number = +component.nativeElement.id.match(/\d+$/);
+      const cancelClick = this.renderer.listen(element, 'click', () => {
+        cancelClick();
+
+        this.renderer.setProperty(element, 'disabled', 'true');
+          Observable.of(this.response)
+            .delay(2000)
+            .subscribe( (data) => {
+              this.hits[index]["subHits"] = data;
+              this.renderer.setProperty(element, 'disabled', null);
+            })
+
+          console.log('should not click any more');
+        });
+    })
   }
 }
