1a790f841ae8a4c61fcd945efef9a9e6e934f78b
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-05-24 18:07:33 +0300

    super optimizations. the toggleSubHits only gets called once.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -8,6 +8,10 @@ import { Observable } from 'rxjs/Rx';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  private response = [
+    { title: "Potoci", count: 13 },
+    { title: "Molfaitori", count: 7
+  }];
 
   hits = [
     {
@@ -25,23 +29,14 @@ export class ListComponent {
   constructor() { }
 
   toggleSubHits(event, hit) {
-    const response = [
-      { title: "Potoci", count: 13 },
-      { title: "Molfaitori", count: 7
-      }];
-
-    console.log('toggled', event);
-
     const element: HTMLInputElement = event.target;
 
-    if(element.checked && !hit.subHits) {
-      element.disabled = true;
-      Observable.of(response)
-          .delay(2000)
-          .subscribe( function(x) {
-            hit.subHits = x;
-            element.disabled = false
-          })
-    }
+    element.disabled = true;
+    Observable.of(this.response)
+        .delay(2000)
+        .subscribe( function(x) {
+          hit.subHits = x;
+          element.disabled = false
+        })
   }
 }
