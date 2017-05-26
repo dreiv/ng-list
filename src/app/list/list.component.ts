e5658083e102cc0879ac4a6d3d84422b4e0a10b9
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-05-26 16:05:27 +0300

    turns out I need a label for the checkbox to work as needed.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -42,7 +42,7 @@ export class ListComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.toggles.forEach(component => {
       const toggle = component.nativeElement;
-      const index:number = +toggle.getAttribute('data-index');
+      const index:number = +toggle.id.match(/\d+$/);
       const cancelClick = this.renderer.listen(toggle, 'click', () => {
         cancelClick();
 
